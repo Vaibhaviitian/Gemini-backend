ed84bfd9a0eda5f3faa19d3ecdc9a98f297abe29
Dhruv <mradhruv460@gmail.com>
2024-08-06 01:10:37 +0530

    added login and signup login i frontend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import cors from 'cors' 
+
 import mongoose from 'mongoose'
 import { app } from "./app.js";
 dotenv.config();
@@ -8,7 +8,7 @@ dotenv.config();
 const port = 3000;  //after compeletion , we will push this  into .env 
 const mongo_uri = 'mongodb+srv://mradhruv460:quiz_dhruv@cluster0.8iwgdu0.mongodb.net/gemini_project'
 
-app.use(cors());
+
 mongoose.connect(mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
