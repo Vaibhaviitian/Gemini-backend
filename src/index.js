e2ec0ef34ce81bdc1f59736a2c63ce7381e9ec19
Vaibhaviitian <aryanvaibhav74@gmail.com>
2024-08-06 23:18:40 +0530

    klko

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import { app } from "./app.js";
 dotenv.config();
 
 
-const port = 1250;  //after compeletion , we will push this  into .env 
 const mongo_uri = 'mongodb+srv://mradhruv460:quiz_dhruv@cluster0.8iwgdu0.mongodb.net/gemini_project'
 // const mongo_uri = 'mongodb+srv://gemini:123@cluster0.zlp0toa.mongodb.net/gemini'
+// everybodu add your port in your .env file 
 
 
 mongoose.connect(mongo_uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log(err));
 
-app.listen(port,()=>{
-console.log(`⚙️ Server is running at ${port} `);
+app.listen(process.env.PORT,()=>{
+console.log(`⚙️ Server is running at ${process.env.PORT} `);
 })
 
