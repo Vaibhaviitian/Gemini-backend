ac3627c81228b0adf4795adf041626bad11af03b
anirban2005143a <157496311+anirban2005143a@users.noreply.github.com>
2024-08-31 01:04:47 +0530

    Add files via upload

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,7 @@
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 import { generateToken } from '../middleware/authMiddleware.js';
-
+import sendMail from '../utility/sendmail.js';
 
 
 export const registerUser = async (req, res) => {
@@ -17,10 +17,10 @@ export const registerUser = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-   
-    user = new User({ username, email, password: hashedPassword});
+
+    user = new User({ username, email, password: hashedPassword });
     await user.save();
-console.log(`user saved`);
+    console.log(`user saved`);
     res.status(201).json({
       userId: user._id,
       username: user.username,
@@ -37,21 +37,21 @@ console.log(`user saved`);
 // Login user
 export const loginUser = async (req, res) => {
   const { email, password, socketId } = req.body;
-//   console.log('Logging in user with socketId:', socketId);
+  //   console.log('Logging in user with socketId:', socketId);
   try {
-    
+
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
 
-  
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
 
-    
+
     const token = generateToken(user._id);
 
     res.json({
@@ -59,11 +59,18 @@ export const loginUser = async (req, res) => {
       username: user.username,
       email: user.email,
       token,
-    
+
     });
   } catch (error) {
     res.status(500).json({ message: 'founded Server error' });
   }
 };
 
-
+export const contact = async (req, res) => {
+  const result = await sendMail(req.body.email, req.body.content)
+  if (result) {
+    return res.json({ error: false, message: 'Mail send successfully !!!' })
+  } else {
+    return res.json({ error: true, status: 500, message: "Some internal error occured , Please try again" });
+  }
+}
